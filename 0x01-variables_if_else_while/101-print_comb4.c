75b4187af3bd64e98e8355a971e3a95eb01e0d55
Driisonims <ibrahimidrisonims@gmail.com>
2022-09-19 15:22:27 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,38 +1,31 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all possible different combinations of
- * three digits
- * Numbers must be separated by ,followed by a space
- * The two digits must be different
- * 012, 120, 102, 021, 201, 210 are considered the same combination
- * Return: 0
+ * main - Entry point
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 {
-int n1 = 48;
-int n2, n3;
-int com = 44;
-while (n1 <= 57)
+int i, j, k;
+for (i = 48; i < 58; i++)
 {
-n2 = n1 + 1;
-while (n2 <= 57)
-n3 = n2 + 1;
-while (n3 <= 57)
+for (j = 49; j < 58; j++)
 {
-putchar(n1);
-putchar(n2);
-putchar(n3);
-if (n1 != 55 || n2 != 56 || n3 != 57)
+for (k = 50; k < 58; k++)
 {
-putchar(com);
-putchar(32);
+if (k > j && j > i)
+{
+putchar(i);
+putchar(j);
+putchar(k);
+if (i != 55 || j != 56)
+{
+putchar(',');
+putchar(' ');
 }
-n3 += 1;
 }
-n2 += 1;
 }
-n1 += 1;
+}
 }
 putchar('\n');
 return (0);
