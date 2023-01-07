a7e7672b7a387686812d25369f3b5e7b7f649fcd
Driisonims <ibrahimidrisonims@gmail.com>
2023-01-06 16:38:04 -0800

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,18 +7,14 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	/* define new hash table */
-	hash_table_t *hashTable = malloc(sizeof(hash_table_t));
-
-	if (!hashTable)
-		return (NULL);
-
-	hashTable->size = size;
-	/* define array of key value pairs (hash nodes) */
-	hashTable->array = malloc(sizeof(hash_node_t *) * size);
-
-	if (hashTable->array == NULL)
-		return (NULL);
-
-	return (hashTable);
+/* define new hash table */
+hash_table_t *hashTable = malloc(sizeof(hash_table_t));
+if (!hashTable)
+return (NULL);
+hashTable->size = size;
+/* define array of key value pairs (hash nodes) */
+hashTable->array = malloc(sizeof(hash_node_t *) * size);
+if (hashTable->array == NULL)
+return (NULL);
+return (hashTable);
 }
